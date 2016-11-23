87fb6bb91f8a46fa6935396f1e59f0e2e273032d
Timothy Smith <psyts9@nottingham.ac.uk>
2016-11-23 10:03:43 +0000

    updated database to connect to the table user.

--- web/php/database.php
+++ web/php/database.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('35.156.58.36', 'evolveUser', 'CapOne5216');
+    $conn = new mysqli('35.156.58.36', 'evolveUser', 'CapOne5216','user');
     if(mysqli_connect_errno()) {
         printf("Connection failed: %s\n", mysqli_connect_error());
         exit();
