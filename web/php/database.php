6eb7aff36b0a93c56242906b345f1df6b7e17fbe
Tudor Sandulescu <psyts7@exmail.nottingham.ac.uk>
2016-11-24 15:56:13 +0000

    Updated some namings + added comments

--- web/php/database.php
+++ web/php/database.php
@@ -1,7 +1,7 @@
 <?php
-    $conn = new mysqli('35.156.58.36', 'evolveUser', 'CapOne5216', 'mobileAuth');
+    $conn = new mysqli('35.156.58.36', 'evolveUser', 'CapOne5216', 'MobileAuth');
     if($conn->connect_errno) {
         printf("Connection failed: %s\n", mysqli_connect_error());
         exit();
     }
- ?>
+ 
--- web/php/handleAddUser.php
+++ web/php/handleAddUser.php
@@ -1,16 +1,17 @@
 <?php
-include 'database.php';
-$email = $_POST["email"];
-$password = $_POST["password"];
-$FName = $_POST["FName"];
-$SName = $_POST["SName"];
-$mobNumber = $_POST["mobNumber"];
+	include 'database.php';
+	
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$FName = $_POST["FName"];
+	$SName = $_POST["SName"];
+	$mobNumber = $_POST["mobNumber"];
 
-$sql = "INSERT INTO user (email, password, firstName, surName, phoneNum) VALUES ('$email', '$password', '$FName', '$SName', '$mobNumber')";
-if ($conn->query($sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+	$sql = "INSERT INTO User (email, password, firstname, surname, mobilenum) VALUES ('$email', '$password', '$FName', '$SName', '$mobNumber')";
+	if ($conn->query($sql)) {
+		echo "New record created successfully\n";
+	} else {
+		echo "Error: " . $sql . "<br>" . $conn->error . "\n";
+	}
 
-$conn->close();
+	$conn->close();
